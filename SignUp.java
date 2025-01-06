bbd22e01eb33381f9c095d90e4e091a0c84246e4
CWXxx <89468607+wanxian23@users.noreply.github.com>
2025-01-06 21:57:28 +0800

    Update SignUp.java

--- SignUp.java
+++ SignUp.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.BufferedWriter;
@@ -136,6 +135,7 @@ public class SignUp {
         JLabel loginLabel = new JLabel("Already have an account? LOG IN");
         loginLabel.setFont(new Font("Arial", Font.PLAIN, 12));
         loginLabel.setForeground(Color.BLUE);
+        loginLabel.addMouseListener(loginClick);
         gbc.gridy = 9;
         gbc.gridx = 0;
         formPanel.add(loginLabel, gbc);
@@ -160,7 +160,7 @@ public class SignUp {
         frameSignup.setLayout(new GridLayout(1, 2));
 
         // Add panels to the frame
-        frameSignup.add(formPanel);
+        frameSignup.add(combineSignupPanels);
         frameSignup.add(imagePanel);
 
         // Set frame visibility
@@ -281,6 +281,14 @@ public class SignUp {
                 return;
             }
 
+        }
+    };
+
+    MouseListener loginClick = new MouseAdapter() {
+        public void mouseClicked(MouseEvent e) {
+            frameSignup.setVisible(false);
+            LogInPage login = new LogInPage();
+            login.createLogInPage();
         }
     };
 
