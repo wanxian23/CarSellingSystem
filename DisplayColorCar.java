e6782a2fff5269ccb1035d162909be1b01b3c72c
CWXxx <89468607+wanxian23@users.noreply.github.com>
2025-01-14 19:07:04 +0800

    Update DisplayColorCar.java

--- DisplayColorCar.java
+++ DisplayColorCar.java
@@ -5,6 +5,13 @@ import java.awt.event.ActionListener;
 
 public class DisplayColorCar {
 
+    String carCategoryChoose;
+    String carModelChoose;
+    String variantChoose;
+    double basePrice;
+    String colorChoose;
+    int motorPowerChoose;
+
     private JFrame frame;
     private JPanel mainPanel;
     private JComboBox<String> colorComboBox;
@@ -13,22 +20,30 @@ public class DisplayColorCar {
     private JPanel buttonPanel;
 
     private String[][] colorVariants = {
-            {"Pearl White Multi-Coat", "Deep Blue Metallic", "Stealth Grey", "Quicksilver", "Ultra Red"},
-            {"Pearl White Multi-Coat", "Deep Blue Metallic", "Stealth Grey", "Solid Black", "Ultra Red"},
-            {"Quicksilver"},
-            {"Pearl White Multi-Coat", "Deep Blue Metallic", "Stealth Grey", "Quicksilver", "Ultra Red"},
-            {"Pearl White Multi-Coat", "Deep Blue Metallic", "Stealth Grey", "Solid Black", "Ultra Red"}
+            {"  -- Select a Color --", "Pearl White Multi-Coat", "Deep Blue Metallic", "Stealth Grey", "Quicksilver", "Ultra Red"},
+            {"  -- Select a Color --", "Pearl White Multi-Coat", "Deep Blue Metallic", "Stealth Grey", "Solid Black", "Ultra Red"},
+            {"  -- Select a Color --", "Quicksilver"},
+            {"  -- Select a Color --", "Pearl White Multi-Coat", "Deep Blue Metallic", "Stealth Grey", "Quicksilver", "Ultra Red"},
+            {"  -- Select a Color --", "Pearl White Multi-Coat", "Deep Blue Metallic", "Stealth Grey", "Solid Black", "Ultra Red"}
     };
 
     private String[][] colorImagePaths = {
-            {"/Images/Color/Aurora/PearlWhiteMulti-Coat.png", "/Images/Color/Aurora/DeepBlueMetallic.png", "/Images/Color/Aurora/StealthGrey.png", "/Images/Color/Aurora/Quicksilver.png", "/Images/Color/Aurora/UltraRed.png"},
-            {"/Images/Color/Imperial/PearlWhiteMulti-Coat.png", "/Images/Color/Imperial/DeepBlueMetallic.png", "/Images/Color/Imperial/StealthGrey.png", "/Images/Color/Imperial/SolidBlack.png", "/Images/Color/Imperial/UltraRed.png"},
-            {"/Images/Color/PowerHaul/Quicksilver.png"},
-            {"/Images/Color/Stratos/PearlWhiteMulti-Coat.png", "/Images/Color/Stratos/DeepBlueMetallic.png", "/Images/Color/Stratos/StealthGrey.png", "/Images/Color/Stratos/Quicksilver.png", "/Images/Color/Stratos/UltraRed.png"},
-            {"/Images/Color/TerraVolt/PearlWhiteMulti-Coat.png", "/Images/Color/TerraVolt/DeepBlueMetallic.png", "/Images/Color/TerraVolt/StealthGrey.png", "/Images/Color/TerraVolt/SolidBlack.png", "/Images/Color/TerraVolt/UltraRed.png"}
+            {"/Images/Color/NoColor.png", "/Images/Color/Aurora/PearlWhiteMulti-Coat.png", "/Images/Color/Aurora/DeepBlueMetallic.png", "/Images/Color/Aurora/StealthGrey.png", "/Images/Color/Aurora/Quicksilver.png", "/Images/Color/Aurora/UltraRed.png"},
+            {"/Images/Color/NoColor.png", "/Images/Color/Imperial/PearlWhiteMulti-Coat.png", "/Images/Color/Imperial/DeepBlueMetallic.png", "/Images/Color/Imperial/StealthGrey.png", "/Images/Color/Imperial/SolidBlack.png", "/Images/Color/Imperial/UltraRed.png"},
+            {"/Images/Color/NoColor.png", "/Images/Color/PowerHaul/Quicksilver.png"},
+            {"/Images/Color/NoColor.png", "/Images/Color/Stratos/PearlWhiteMulti-Coat.png", "/Images/Color/Stratos/DeepBlueMetallic.png", "/Images/Color/Stratos/StealthGrey.png", "/Images/Color/Stratos/Quicksilver.png", "/Images/Color/Stratos/UltraRed.png"},
+            {"/Images/Color/NoColor.png", "/Images/Color/TerraVolt/PearlWhiteMulti-Coat.png", "/Images/Color/TerraVolt/DeepBlueMetallic.png", "/Images/Color/TerraVolt/StealthGrey.png", "/Images/Color/TerraVolt/SolidBlack.png", "/Images/Color/TerraVolt/UltraRed.png"}
     };
 
-    public DisplayColorCar(String[] selectedVariantPrice, int selectedCarIndex) {
+    public void colorCarPage(String carCategoryChoose, String carModelChoose,
+                             String variantChoose, double basePrice, int motorPowerChoose, String[] selectedVariantPrice, int selectedCarIndex) {
+
+        this.carCategoryChoose = carCategoryChoose;
+        this.carModelChoose = carModelChoose;
+        this.variantChoose = variantChoose;
+        this.basePrice = basePrice;
+        this.motorPowerChoose = motorPowerChoose;
+
         frame = new JFrame("EcoMotion Car Color");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -49,6 +64,7 @@ public class DisplayColorCar {
         JPanel comboBoxPanel = new JPanel();
         comboBoxPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 30,10));
         colorComboBox = new JComboBox<>(colorVariants[selectedCarIndex]);
+        colorComboBox.setSelectedIndex(0);
         colorComboBox.setFont(new Font("Arial", Font.PLAIN, 20));
         colorComboBox.setPreferredSize(new Dimension(300, 40));
         comboBoxPanel.add(colorComboBox);
@@ -80,11 +96,17 @@ public class DisplayColorCar {
 
         JButton backButton = new JButton("Back");
         backButton.setFont(new Font("Arial", Font.PLAIN, 35));
+        backButton.setForeground(Color.WHITE);
+        backButton.setBackground(Color.black);
         backButton.addActionListener(e -> showPreviousPage());
         buttonPanel.add(backButton);
 
         JButton confirmButton = new JButton("Confirm");
         confirmButton.setFont(new Font("Arial", Font.PLAIN, 35));
+        confirmButton.setForeground(Color.WHITE);
+        confirmButton.setBackground(Color.black);
+        confirmButton.addActionListener(confirmButtonEvent);
+
         buttonPanel.add(confirmButton);
 
         mainPanel.add(buttonPanel);
@@ -93,19 +115,44 @@ public class DisplayColorCar {
         frame.setVisible(true);
     }
 
-    private void updateColorImage(int selectedCarIndex) {
-        String selectedColor = (String) colorComboBox.getSelectedItem();
-        int colorIndex = colorComboBox.getSelectedIndex();
-        ImageIcon colorIcon = new ImageIcon(getClass().getResource(colorImagePaths[selectedCarIndex][colorIndex]));
+    ActionListener confirmButtonEvent = new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            int selectedColorIndex = colorComboBox.getSelectedIndex();
 
-        Image img = colorIcon.getImage();
-        Image scaledImg = img.getScaledInstance(1000, 400, Image.SCALE_SMOOTH);
-        colorIcon = new ImageIcon(scaledImg);
-        carColorImageLabel.setIcon(colorIcon);
+            if (selectedColorIndex == 0) {
+
+                JOptionPane.showMessageDialog(null, "Please select a color!", "Error! No color choose!", JOptionPane.WARNING_MESSAGE);
+
+            } else {
+                frame.setVisible(false);
+                PaymentSummaryPage paymentSumPage = new PaymentSummaryPage();
+                paymentSumPage.showPaymentSummaryPage(carCategoryChoose, carModelChoose,
+                        variantChoose, basePrice, colorChoose, motorPowerChoose);
+            }
+        }
+    };
+
+    private void updateColorImage(int selectedCarIndex) {
+
+        int selectedColorIndex = colorComboBox.getSelectedIndex();
+
+        if (selectedColorIndex != 0) {
+            String selectedColor = (String) colorComboBox.getSelectedItem();
+            int colorIndex = colorComboBox.getSelectedIndex();
+            ImageIcon colorIcon = new ImageIcon(getClass().getResource(colorImagePaths[selectedCarIndex][colorIndex]));
+
+            Image img = colorIcon.getImage();
+            Image scaledImg = img.getScaledInstance(1000, 400, Image.SCALE_SMOOTH);
+            colorIcon = new ImageIcon(scaledImg);
+            carColorImageLabel.setIcon(colorIcon);
+
+            colorChoose = selectedColor;
+        }
     }
 
     private void showPreviousPage() {
         frame.dispose();
-        new Description();
+        Description descriptionPage = new Description();
+        descriptionPage.showDescriptionPage();
     }
 }
